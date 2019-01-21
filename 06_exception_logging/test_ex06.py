276f04194ec1f40eb6bb9bd3978c182d02ae5af9
git piyopiyoml <piyopiyoml.contact@gmail.com>
2019-01-21 17:39:51 +0900

    add test ex06

---
+++ 06_exception_logging/test_ex06.py
@@ -0,0 +1,29 @@
+from logging import getLogger, StreamHandler, DEBUG
+
+
+def create_logger(name, log_level):
+    """create logger"""
+    logger = getLogger(name if name else __file__)
+    handler = StreamHandler()
+    handler.setLevel(log_level)
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
+    return logger
+
+
+def main():
+    """show how to handle and log the exception"""
+
+    logger = create_logger("my_logger", DEBUG)
+    logger.info("Now try to open file that does not exist!")
+
+    #WRITE YOUR CODE HERE: below script will cause the Exception!
+    try:
+        with open("not_exist_file.txt") as f:
+            lines = f.readlines()
+    except IOError as ex:
+        # WRITE YOUR CODE HERE: handle the exception
+        logger.critical("You faced Exception! {}".format(ex))
+
+if __name__ == "__main__":
+    main()
